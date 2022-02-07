9f2a7c434bca66d8e1ae15b6453155b32c6b3452
krzysztof.marchewka <krzysztof.marchewkaa@gmail.com>
2022-02-07 01:33:46 +0100

    dokonczenie dataloader

--- src/models/EpsilonDataset.py
+++ src/models/EpsilonDataset.py
@@ -8,7 +8,8 @@ from tifffile import imread
 class EpsilonDataset(torch.utils.data.Dataset):
     def __init__(self, root_dir, annotation_file, transform=None):
         self.root_dir = root_dir
-        self.annotations = pd.read_csv(annotation_file,skiprows=1)
+        #self.annotations = pd.read_csv(annotation_file,skiprows=1)
+        self.annotations = pd.read_csv(annotation_file,skiprows=0)
         self.transform = transform
 
     def __len__(self):
@@ -25,3 +26,16 @@ class EpsilonDataset(torch.utils.data.Dataset):
 if __name__ == "__main__":
     x = EpsilonDataset("../../data/processed", "../../data/data.csv")
     print(x.__getitem__(0))
+    
+#Test
+from __init__ import Config
+import torchvision.transforms as transforms
+config = Config()
+data_transform = transforms.Compose([
+    transforms.CenterCrop(488),
+    transforms.Resize(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                          std=[0.229, 0.224, 0.225])
+])
+dataset = EpsilonDataset(config.data_root_dir, config.dataset_metadata, transform=data_transform)
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -22,15 +22,27 @@ np.random.seed(0)
 
 
 def get_stratified_sampler(config, generator):    
-    df = pd.read_csv(config.dataset_metadata) #read data from CSV to var df
-    cartridges_names = list(df.folder.unique())
-    test_cartridge = cartridges_names.pop(np.random.randint(0, len(cartridges_names)))
-    test_index = df[df.folder.str.match(test_cartridge)].index.to_numpy()
-    train_df = df[~df.index.isin(test_index)]
-    y = train_df[['compound_label']].to_numpy()
-    x = np.array(list(range(len(y)))).reshape(-1, 1)
-    sss = StratifiedShuffleSplit(n_splits=1, test_size=0.1, random_state=0)
+    df = pd.read_csv(config.dataset_metadata) #read data from CSV to var "Data Frame"
+    
+    #cartridges_names = list(df.folder.unique()) #????
+    cartridges_names = list(df.folds.unique()) #tworzy liste unikalnych wartosci
+    
+    test_cartridge = cartridges_names.pop(np.random.randint(0, len(cartridges_names))) #losuje jedna unikalna wartosc
+    
+    #test_index = df[df.folder.str.match(test_cartridge)].index.to_numpy()
+    test_index = df[df.folds.str.match(test_cartridge)].index.to_numpy() #wyciaga wszystkie indeksy dla wartosci wyznaczonej wczesniej
+    
+    train_df = df[~df.index.isin(test_index)] #wywala z tablicy wszystkie wartosci okreslone jako testowe
+    #y = train_df[['compound_label']].to_numpy()
+    y = train_df[['epsilon']].to_numpy().astype(float) #zbior danych do uczenia
+    x = np.array(list(range(len(y)))).reshape(-1, 1) #tworzy tablice o wielkosci zbioru uczacego
+    sss = StratifiedShuffleSplit(n_splits=1, test_size=0.1, random_state=0) #do podzialu. pozniej mozna ustawic wiecej splitow
     (train_index, val_index) = list(sss.split(x, y))[0]
+    # for train_index, val_index in sss.split(x, y):
+    #     print("TRAIN:", train_index, "TEST:", test_index)
+    #     x_train, x_val = x[train_index], x[val_index]
+    #     y_train, y_val = y[train_index], y[val_index]
+    #generator = torch.Generator(device=device).manual_seed(0)
     train_sampler = torch.utils.data.SubsetRandomSampler(train_index, generator=generator)
     val_sampler = torch.utils.data.SubsetRandomSampler(val_index, generator=generator)
     test_sampler = torch.utils.data.SubsetRandomSampler(test_index, generator=generator)
@@ -55,13 +67,13 @@ data_transform = transforms.Compose([
     transforms.Resize(224),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                          std=[0.229, 0.224, 0.225])
+                          std=[0.229, 0.224, 0.225]) #jak to dziala
 ])
 
 print(data_transform)
 #for test
 dataset = EpsilonDataset(config.data_root_dir, config.dataset_metadata, transform=data_transform)
-first_data = dataset[3]
+first_data = dataset[2]
 features, labels = first_data
 #end test
 
