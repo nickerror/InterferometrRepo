06ac0cc1ceb2a9daf278baafa02d1f62f65d792d
krzysztof.marchewka <krzysztof.marchewkaa@gmail.com>
2022-07-22 00:17:24 +0200

    model with used hardtang as activate function. Add tensorboard

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -9,18 +9,16 @@ from model_functions.Config import Config
 from model_functions.PathManagement import PathManagement
 from model_functions.data_for_model import prepare_data
 from model_functions.loss_function import numpy_single_custom_loss_function
+from captum.attr import IntegratedGradients
 
-
-# pathManagement=PathManagement(dataType="original", 
-#                                 noiseType="noised", 
-#                                 centerInTheMiddle=True, 
-#                                 purposeData="test")
-
-pathManagement=PathManagement(dataType="original", noiseType="noised", centerInTheMiddle=True, purposeData="test")
+pathManagement=PathManagement(dataType="original",
+                                noiseType="noised",
+                                centerInTheMiddle=True,
+                                purposeData="test")
 
 
 config=Config(pathManagement)
-config.setModelNameToRead("test_hardtanh_2.pth")
+config.setModelNameToRead("hardtanh_1_generated_mixed.pth")
 
 
 ###################################################################################################################
@@ -41,19 +39,11 @@ j=0
 #############################################_charts etc_##############################################################
 ##### with printing charts
 
-#device="cpu"
-
 config.batch_size=1
 dataloaders = prepare_data(config, train=False)
-#device="cuda"
 allDiffs=0.0
 stats=Stats(binCount = 100)
 j=0
-
-numberOfSamples = np.zeros([100,1],dtype=int) #.
-minError = np.ones([100,1],dtype=float)#.
-maxError = np.zeros([100,1],dtype=float) #.
-tempBinNumber = 0 #.
 
 
 for images, labels in dataloaders['test']:
@@ -64,17 +54,11 @@ for images, labels in dataloaders['test']:
     diff = numpy_single_custom_loss_function(output = outputs[0], label = labels[0])
 
     label=copy.deepcopy(float(labels[0]))
-    #output=copy.deepcopy(float(outputs[0][0])) #for sigmoid
-    output=copy.deepcopy(float(outputs[0])) #for linear activation
+    output=copy.deepcopy(float(outputs[0]))
     allDiffs+=diff
     j+=1
     stats.AddCalculation(label, diff)
     stats.statistics.addReturnedStatistics(output=output, label = label, lossCalculatedError=diff)
-    #print("label: ", label,"diff: ", diff) #.
-    
-    #print("label ", j, " = ", label)
-    #print("output ", j, " = ", output)
-    #print("loss return ", j, " = ", diff)
 
     if (j%100==0): print(j, "mean:", allDiffs/j)
 
